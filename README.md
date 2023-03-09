87a38d4cfbbeb35e175f5a0de90e7d1cadf7a063
Keita Tashiro <k-tashiro@mba-kt.local>
2023-03-10 01:41:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+参考URL
+https://zenn.dev/thirosue/books/49a4ee418743ed/viewer/ae5b9a
---
+++ prisma/index.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // TODO ここに処理を記載する
+  // const alice = await prisma.user.findUnique({
+  //   where: { email: 'alice@example.com' }
+  // })
+
+  // console.log(alice)
+
+  // const users = await prisma.user.findMany()
+
+  // console.log(users)
+
+  const bob = await prisma.user.findUnique({
+    where: {email: 'bob@example.com'},
+    include: { posts: true }
+  })
+
+  console.log(bob)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+
